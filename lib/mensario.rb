cf305a505f2383c6af2e6b5cf56493c1a8b8a608
Javier Aranda <jaranda@nosolosoftware.biz>
2011-05-17 10:16:01 +0200

    Implementada una función para generar la petición xml al api

--- lib/mensario.rb
+++ lib/mensario.rb
@@ -1,5 +1,6 @@
 require 'mensario/status'
 require 'mensario/exception'
+require 'xmlsimple'
 
 module Mensario
   class Mensario
@@ -13,6 +14,26 @@ module Mensario
  
     def initialize
       yield self
+    end
+
+    # Create the request xml for api call with basic options
+    #
+    # @param [String] task name of the api task
+    # @param [Hash] options a hash containing the other data to add in api call using the xmlsimple format
+    # @return [String] xml formatted ready for api call
+    def create_xml(task, options = {})
+      #default xml structure
+      xml = { 'task' => ["#{task}"],
+              'license' => {
+                'number' => @license,
+                'user'   => @username,
+                'pass'   => @password
+              }
+      }
+
+      xml = xml.merge options
+
+      XmlSimple.xml_out(xml, :rootname => 'api', :XmlDeclaration => '<?xml version="1.0" encoding="UTF-8"?>')
     end
     
     def synchronize
