1bc212bcff90c1e059d7ac2e8638a8eb6394655d
Juan Antonio Galisteo Prieto <jgalisteo@nosolosoftware.biz>
2011-05-19 13:13:42 +0200

    Añadida la funcionalidad del método 'license_query'. Resuelve #162

--- lib/mensario.rb
+++ lib/mensario.rb
@@ -68,9 +68,11 @@ module Mensario
       response = api_call(xml)
     end
 
-    def send_message(prefix, phone, message)
+    def send_message()
     end
 
+    # Allows to consult the quantity remaining
+    # @result [Fixnum] the quantity remaining of the license
     def quantity
       xml = { 'task' => ['QUANT-QRY'],
               'license' => {
@@ -83,22 +85,42 @@ module Mensario
       api_call(xml)['quantity'].first.to_i
     end
 
-    def license_query
-      xml = { 'task' => ['LIC-QRY'],
-              'licenses' => {
-                'license' => {
-                  'number'  => @license,
-                  'user'    => @username,
-                  'pass'    => @password
-                }
-              }
+    # Allows to consult the status, quantity and type of one or
+    # more licenses
+    # @param [Array] hash array with extra licenses
+    # @result [Array] Response of the API
+    # @example extra licenses
+    #   extra = [
+    #     { 'number' => number-1, 'user' => user-1, 'pass' => pass-1 },
+    #       'number' => number-2, 'user' => user-2, 'pass' => pass-2 },
+    #                                 .
+    #                                 .
+    #                                 .
+    #       'number' => number-n, 'user' => user-n, 'pass' => pass-n }
+    #   [
+    # @example response
+    #   [
+    #     { "number" => "LSTD020801EUHNKOQIHJ", "status" => ["OK"], "quantity"=> ["1000"], "type" => ["STD"] }
+    #     { "number" => "STD000001EAAAIIOOLP",  "status" => ["KO-INV"] } 
+    #   ]
+    def license_query(opts = [])
+      licenses = {
+        'license' => [{
+          'number'  => @license,
+          'user'    => @username,
+          'pass'    => @password
+        }]
       }
 
-      response = api_call(xml)['licenses'].first['license'].first
-      hash = { 'status' => response['status'].first }
-      hash.merge!({ 'quantity' => response['quantity'].first.to_i }) if response['quantity']
-      hash.merge!({ 'type' => response['type'].first }) if response['type']
-      hash
+      opts.length.times do |i|
+        licenses['license'] << opts[i]
+      end
+
+      xml = { 'task' => ['LIC-QRY'],
+              'licenses' => licenses
+      }
+
+      api_call(xml)['licenses'].first['license']
     end
 
     def request_query(request_id)
