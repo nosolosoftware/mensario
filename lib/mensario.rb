441e2dc5ccc8e2abb27a17a1ccfe5482ee6a259c
Javier Aranda <jaranda@nosolosoftware.biz>
2011-05-25 13:49:46 +0200

    Añadido yaml en los requires

--- lib/mensario.rb
+++ lib/mensario.rb
@@ -17,6 +17,7 @@
 # You should have received a copy of the GNU General Public License
 # along with Runnable.  If not, see <http://www.gnu.org/licenses/>.
 
+require 'yaml'
 require 'mensario/exception'
 require 'xmlsimple'
 require 'net/http'
